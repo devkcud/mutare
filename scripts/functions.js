3bd5bb6ca8356dd7157b3c57712594ab28528cef
André Luis (devkcud) <andrescalisejr@gmail.com>
2023-06-23 16:53:24 -0300

    feat!: A bunch of new stuff
    New keybinds, new functions, rework on previously made functions, etc

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,10 +1,43 @@
-function getTime() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
+function newfile() {
+    editor.value = '';
+}
 
-    return `${hours}:${minutes}:${seconds}`;
+function openfile() {
+    element = document.createElement('input');
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.type = 'file';
+    element.click();
+
+    element.addEventListener('change', (e) => {
+        const file = e.target.files[0];
+
+        new Promise((resolve, reject) => {
+            const reader = new FileReader();
+
+            reader.onload = (e) => resolve(e.target.result);
+            reader.onerror = (e) => reject(e.target.error);
+
+            reader.readAsText(file);
+        })
+            .then((content) => editor.value = content)
+            .catch((error) => console.error('Error:', error));
+    });
+
+    document.body.removeChild(element);
+}
+
+function savefile() {
+    element = document.createElement('a');
+    element.style.display = 'none';
+
+    element.setAttribute('href', `data:text/plain;charset=utf-8,${encodeURIComponent(editor.value)}`);
+    element.setAttribute('download', 'text.md');
+
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
 }
 
 function notify(title, msg, urgent) {
@@ -19,15 +52,15 @@ function notify(title, msg, urgent) {
 
     head.innerHTML = title;
     body.innerHTML = msg;
-    hour.innerHTML = getTime();
+
+    const now = new Date();
+    hour.innerHTML = `${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}`;
 
     li.appendChild(head);
     li.appendChild(body);
     li.appendChild(hour);
 
-    li.addEventListener('click', (e) => {
-        notifications.removeChild(li);
-    });
+    li.addEventListener('click', () => notifications.removeChild(li));
 
     notifications.appendChild(li);
 
@@ -46,63 +79,35 @@ function notify(title, msg, urgent) {
                 try { notifications.removeChild(li); } catch (e) {}
                 clearInterval(interval);
             }
-        }, 100);
-    }, 5000);
+        }, 50);
+    }, 3000);
 }
 
 function clearNotifications() {
     notifications.innerHTML = '';
 }
 
-function toggle(open) {
+function toggleWrap() {
+    editor.style.textWrap = (editor.style.textWrap === 'nowrap' ? 'wrap' : 'nowrap');
+    notify('Wrap', 'Text wrapping ' + (editor.style.textWrap === 'nowrap' ? 'disabled' : 'enabled'))
+}
+
+function addText(open, close = open) {
     const start = editor.selectionStart;
     const end = editor.selectionEnd;
 
-    editor.value = editor.value.substring(0, start) + open + editor.value.substring(start, end) + open + editor.value.substring(end);
+    editor.value = editor.value.substring(0, start) + open + editor.value.substring(start, end) + close + editor.value.substring(end);
 
     editor.focus();
     editor.selectionEnd = end + open.length;
 }
 
-function changewrap() {
-    editor.style.textWrap = (editor.style.textWrap === 'nowrap' ? 'wrap' : 'nowrap');
-    notify('Wrap', 'Text wrapping ' + (editor.style.textWrap === 'nowrap' ? 'disabled' : 'enabled'))
-}
+function addTextStart(open) {
+    const start = editor.selectionStart;
+    const end = editor.selectionEnd;
 
-function savefile() {
-    const element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(editor.value));
-    element.setAttribute('download', 'text.md');
+    editor.value = open + editor.value.substring(0);
 
-    element.style.display = 'none';
-
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
-}
-
-function openfile() {
-    const hidden = document.createElement('input');
-    hidden.type = 'file';
-    hidden.style.display = 'none';
-
-    document.body.appendChild(hidden);
-    hidden.click();
-
-    hidden.addEventListener('change', (e) => {
-        const file = e.target.files[0];
-
-        new Promise((resolve, reject) => {
-            const reader = new FileReader();
-
-            reader.onload  = (e) => resolve(e.target.result);
-            reader.onerror = (e) => reject(e.target.error);
-
-            reader.readAsText(file);
-        })
-            .then((content) => (editor.value = content))
-            .catch((error) => console.error('Error:', error));
-    });
-
-    document.body.removeChild(hidden);
+    editor.focus();
+    editor.selectionEnd = end + open.length;
 }
